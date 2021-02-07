00b459bd344a1d98552bbd496378f84a8497103b
Dave <davidjamescottrell@gmail.com>
2021-02-07 00:23:40 +0000

    Added uuid package to auto generate keys for projects

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -24,13 +24,12 @@ class Projects extends Component {
                         title = "Wribbenhall School"
                         infoElements = {
                             [
-                                <p key={10}>This is the website I developed for a private special-education school. This was my second professional project and the first large application I had built with modern web development tools.</p>,
-                                <p key={11}>As my employers essentially gave me complete creative freedom, I really enjoyed the creative element of this project.</p>,
-                                <a key={12} href="https://github.com/DavidJCottrell/WribbenhallReactApp" rel="noreferrer" target="_blank"> - View On GitHub</a>,
-                                <a key={13} href="https://wribbenhallschool.co.uk/" rel="noreferrer" target="_blank"> - View Live Site</a> 
+                                <p>This is the website I developed for a private special-education school. This was my second professional project and the first large application I had built with modern web development tools.</p>,
+                                <p>As my employers essentially gave me complete creative freedom, I really enjoyed the creative element of this project.</p>,
+                                <a href="https://github.com/DavidJCottrell/WribbenhallReactApp" rel="noreferrer" target="_blank"> - View On GitHub</a>,
+                                <a href="https://wribbenhallschool.co.uk/" rel="noreferrer" target="_blank"> - View Live Site</a> 
                             ]
                         }
-                        id = {14}
                         delay = {200}
                     />
 
@@ -38,11 +37,10 @@ class Projects extends Component {
                         title = "MNIST TensorFlow"
                         infoElements = {
                             [
-                                <p key={20}>After recently developing an interest in Machine Learning, I decided to begin learning TensorFlow and Machine Learning fundamentals. This is my first project using TensorFlow.</p>,
-                                <a key={21} href="https://github.com/DavidJCottrell/MNIST-TensorFlow" rel="noreferrer" target="_blank"> - View On GitHub</a>
+                                <p>After recently developing an interest in Machine Learning, I decided to begin learning TensorFlow and Machine Learning fundamentals. This is my first project using TensorFlow.</p>,
+                                <a href="https://github.com/DavidJCottrell/MNIST-TensorFlow" rel="noreferrer" target="_blank"> - View On GitHub</a>
                             ]
                         }
-                        id = {22}
                         delay = {300}
                     />
 
@@ -50,12 +48,11 @@ class Projects extends Component {
                         title = "Java Game and Game Engine"
                         infoElements = {
                             [
-                                <p key={30}>This project was developed as coursework for one of my first University Modules.</p>,
-                                <p key={31}>The game was built from scratch using only Java's built-in graphics libraries.</p>,
-                                <a key={32} href="https://github.com/DavidJCottrell/2D-Java-game" rel="noreferrer" target="_blank"> - View On GitHub</a>
+                                <p>This project was developed as coursework for one of my first University Modules.</p>,
+                                <p>The game was built from scratch using only Java's built-in graphics libraries.</p>,
+                                <a href="https://github.com/DavidJCottrell/2D-Java-game" rel="noreferrer" target="_blank"> - View On GitHub</a>
                             ]
                         }
-                        id = {33}
                         delay = {400}
                     />
 
@@ -63,11 +60,10 @@ class Projects extends Component {
                         title = "Conway's Game of Life"
                         infoElements = {
                             [
-                                <p key={40}>A simple implementation of Conway's Game of Life using C++ and SDL2.</p>,
-                                <a key={41} href="https://github.com/DavidJCottrell/SDL2-Game-Of-Life" rel="noreferrer" target="_blank"> - View On GitHub</a>
+                                <p>A simple implementation of Conway's Game of Life using C++ and SDL2.</p>,
+                                <a href="https://github.com/DavidJCottrell/SDL2-Game-Of-Life" rel="noreferrer" target="_blank"> - View On GitHub</a>
                             ]
                         }
-                        id = {42}
                         delay = {500}
                     />
 
